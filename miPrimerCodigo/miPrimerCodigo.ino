873bdb9735096e452341963dbb93442be005b396
valetiznado <164402604+valetiznado@users.noreply.github.com>
2024-03-21 17:56:46 -0300

    Add files via upload

---
+++ miPrimerCodigo/miPrimerCodigo.ino
@@ -0,0 +1,13 @@
+// 2024-03-21
+// primer arduino
+// queremos  que este codigo prenda y apague una luz
+// por valetiznado
+// hecho con Arduino IDE 2.2.2
+// para Arduino UNO R3
+
+
+void setup()
+{
+
+}
+void loop()
