ed4ad50637e8bfdaf2fb08c71147a282fa550147
Julio San Martín <103392227+SrYuyo@users.noreply.github.com>
2024-03-21 19:28:44 -0300

    Update codigoC3.ino

--- estudiantes/SrYuyo/clase-03/codigoC3/codigoC3.ino
+++ estudiantes/SrYuyo/clase-03/codigoC3/codigoC3.ino
@@ -1,20 +1,20 @@
-// Definición del pin del botón y del pin del LED
+// Definición del pin del boton y del pin del LED
 const int botonPin = 2;       
 
-// Variable para almacenar el estado del botón
+// Variable para almacenar el estado del boton
 int estadoBoton;
-// Variable para almacenar el estado anterior del botón
+// Variable para almacenar el estado anterior del boton
 int estadoBotonAnterior = LOW;
 
 void setup() {
-  // Inicializar el pin del botón como entrada
+  // Inicializar el pin del boton como entrada
   pinMode(botonPin, INPUT);
   // Inicializar la comunicación serial a 9600 baudios
   Serial.begin(9600);
 }
 
 void loop() {
-  // Leer el estado actual del botón
+  // Leer el estado actual del boton
   estadoBoton = digitalRead(botonPin);
 
   // Si el botón ha sido presionado
@@ -23,7 +23,7 @@ void loop() {
     Serial.println("boton presionado");
   }
 
-  // Actualizar el estado anterior del botón
+  // Actualizar el estado anterior del boton
   estadoBotonAnterior = estadoBoton;
 }
 
