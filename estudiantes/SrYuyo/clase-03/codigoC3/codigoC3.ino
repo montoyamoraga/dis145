5147a169d01571e44848250077c53bdb24cbe934
Julio San Martín <103392227+SrYuyo@users.noreply.github.com>
2024-03-21 19:33:09 -0300

    Update codigoC3.ino

--- estudiantes/SrYuyo/clase-03/codigoC3/codigoC3.ino
+++ estudiantes/SrYuyo/clase-03/codigoC3/codigoC3.ino
@@ -1,29 +1,36 @@
-// Definición del pin del boton y del pin del LED
+// 2024-03-21
+// segunda sesión
+// se quiere lograr que un botón al encenderlo que muestre por el monitor si hay click
+// por thiagv y SrYuyo
+// hecho con: Arduino UNO R3 (parche: 2.3.2)
+
+
+// definición del pin del boton y del pin del LED
 const int botonPin = 2;       
 
-// Variable para almacenar el estado del boton
+// variable para almacenar el estado del boton
 int estadoBoton;
-// Variable para almacenar el estado anterior del boton
+// variable para almacenar el estado anterior del boton
 int estadoBotonAnterior = LOW;
 
 void setup() {
-  // Inicializar el pin del boton como entrada
+  // inicializar el pin del boton como entrada
   pinMode(botonPin, INPUT);
-  // Inicializar la comunicación serial a 9600 baudios
+  // inicializar la comunicación serial a 9600 baudios
   Serial.begin(9600);
 }
 
 void loop() {
-  // Leer el estado actual del boton
+  // leer el estado actual del boton
   estadoBoton = digitalRead(botonPin);
 
-  // Si el botón ha sido presionado
+  // si el botón ha sido presionado
   if (estadoBoton == HIGH && estadoBotonAnterior == LOW) {
-    // Mostrar mensaje en el monitor serial
+    // mostrar mensaje en el monitor serial
     Serial.println("boton presionado");
   }
 
-  // Actualizar el estado anterior del boton
+  // actualizar el estado anterior del boton
   estadoBotonAnterior = estadoBoton;
 }
 
