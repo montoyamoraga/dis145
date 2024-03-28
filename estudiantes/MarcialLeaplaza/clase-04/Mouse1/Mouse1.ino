b0d386b120f971788cf09a8215f006a5d48b08ad
MarcialLeaplaza <165319963+MarcialLeaplaza@users.noreply.github.com>
2024-03-28 18:18:38 -0300

    Update Mouse1.ino

--- estudiantes/MarcialLeaplaza/clase-04/Mouse1/Mouse1.ino
+++ estudiantes/MarcialLeaplaza/clase-04/Mouse1/Mouse1.ino
@@ -1,30 +1,33 @@
+//Definicion de pines.
+//Potenciometros:
 int pinPotIzq = A3;
 int pinPotDer = A5;
-int clickDer = 8;
-int clickIzq = 7;
+//Botones para mas adelante:
+int pinClickDer = 8;
+int pinClickIzq = 7;
+//Resolucion de pantalla para el cursor:
 int pixelsX = 800;
 int pixelsY = 600;
 
-
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
-
 void loop() {
-  // put your main code here, to run repeatedly:  
+  // Lectura de señal de potenciometros.
   int potIzq = analogRead(pinPotIzq);
   int potDer = analogRead(pinPotDer);
-  
+
+  //Transformacion de lectura al tamaño de la pantalla.
   int posX = map(potIzq, 0, 1023, 0, pixelsX);
   int posY = map(potDer, 0, 1023, 0, pixelsY);
 
+  //Imprimir en consola la posicion del cursor.
   Serial.print("Posicion del mouse: X: ");
   Serial.print(posX);
   Serial.print(", Y: ");
   Serial.println(posY);
 
+  //Delay para dar tiempo para leer las coordenadas.
   delay(10);
-
 }
