dfaed9f80889e0ee7f82c1f47ff6bffbfa654dd1
aarón montoya-moraga <montoyamoraga@gmail.com>
2024-04-05 13:45:32 -0300

    consolidar y mover contenidos de Spidermaneberl

--- estudiantes/spidermaneberl/clase-03/miPrimerCodigo/miPrimerCodigo.ino
+++ estudiantes/spidermaneberl/clase-03/miPrimerCodigo/miPrimerCodigo.ino
@@ -1,16 +1,29 @@
 // 2024-03-21
 //primer arduinito 
 //prenda y apague luz
-//por Spidermaneberl
+//por Spidermaneberl y 
 //hecho con Rduino IDE2.3.2
 //para arduino UNO R3
 
-void setup() {
-  // put your setup code here, to run once:
+const int buttonPin = 4;     
+const int ledPin =  12;
 
+int buttonState = 0;    
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  buttonState = digitalRead(buttonPin);
 
+  if (buttonState == HIGH) {
+    digitalWrite(ledPin, HIGH);
+  } 
+  else {
+    digitalWrite(ledPin, LOW);
+  }
+  
+  delay(50);
 }
