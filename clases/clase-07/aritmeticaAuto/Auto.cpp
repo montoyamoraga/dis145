8cecd050bd4aa70c1418ccd6f0392bbd9f8a34e7
Janis Sepúlveda <144460794+janisepulveda@users.noreply.github.com>
2024-04-18 19:44:03 -0400

    Add files via upload

---
+++ clases/clase-07/aritmeticaAuto/Auto.cpp
@@ -0,0 +1,21 @@
+
+
+#include "Auto.h"
+
+
+Auto::Auto() {
+  velocidad = 0;
+  combustible = 1.0;
+}
+
+// esta funcion es para que el auto frene
+Auto::frenar() {
+  this.velocidad = 0;
+}
+
+Auto::acelerar() {
+  velocidad++;
+  if (velocidad > 50) {
+    velocidad = 50;
+    }
+}
---
+++ clases/clase-07/aritmeticaAuto/aritmeticaAuto.ino
@@ -0,0 +1,14 @@
+int finanzas;
+int sueldo0 = 3;
+int otroSueldo = 7;
+int deudas = -10000000;
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+ finanzas = sumar3(sueldo0,  otroSueldo, deudas);
+ Serial.println(finanzas); 
+}
